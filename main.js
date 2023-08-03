48592f27f92b90d21ac6572582806dd820a911a5
Rustem Aisariyev <scorus0808@gmail.com>
2023-08-03 14:04:49 -0400

    added html ,css , js files

---
+++ main.js
@@ -0,0 +1,11 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+Console.log(petDog);
+Console.log(petCat);
+Console.log("My pet dog's name is: " + petDog);
+Console.log("My pet cat's name is: " + petCat);
+var catSound = "purr";
+var dogSound = "woof";
+Console.log(petCat + " says " + catSound);
+catSound = "meow";
+Console.log(petCat + " now says " + catSound);
